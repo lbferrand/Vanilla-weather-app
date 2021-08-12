390bd0cc53bdb192182fc71ece1c9cf9949d31c8
lbferrand <lbferrand@gmail.com>
2021-08-12 12:10:48 -0400

    Added api for forecast temperature in app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForescast() {
+function displayForescast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -33,7 +33,7 @@ function displayForescast() {
       <div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         <img
-          src="https://openweathermap.org/img/wn/10d@2x.png"
+          src="http://openweathermap.org/img/wn/10d@2x.png"
           alt=""
           width="36"
         />
@@ -44,6 +44,12 @@ function displayForescast() {
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
   });
+}
+
+function getForecast(coordinates) {
+  let apiKey = "111d251bac5a6929ec7b388bf51f6a63";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForescast);
 }
 
 function displayTemperature(response) {
@@ -70,6 +76,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
